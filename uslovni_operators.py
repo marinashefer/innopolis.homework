d05ac9f803fe6f5b65e7962863afc3bb9080a511
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-05-03 19:01:57 +0300

    смнигтшь

--- uslovni_operators.py
+++ uslovni_operators.py
@@ -39,16 +39,33 @@ print(s[0] +s[-1] + s[4] + s[6])
 print(s[:-1])
 
 d = 'Python'
-for i in range(len(s)):
-    print(d[i])
+for i in range(len(d)):
+    print(d)
 
 for i in d:
     print(d)
 
 for i in range(len(d)):
-    print(d[i])
+    print(d)
 
 # задачка
-s = '0123456789098765432'
-for i in range(0, les(s), 5):
-    print(s[i], end='')
+s = '0123456789098768432'
+for i in range(0, len(s), 5):
+    print(s[i], end=' ')
+
+
+s = 'Hello'
+print(s[::-1])
+print(s[::-2])
+print(s[::-3])
+
+name = 'Tom'
+print(f'{name} Hello')
+
+# задачка!
+word = input('Введите строку:')
+word2 = word[::-1]
+if word == word2:
+    print('строка палиндром')
+else:
+    print('строка не палиндром')
