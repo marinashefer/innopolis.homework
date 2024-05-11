21ddb12c43230e8513d228008fc3d1b3b2b06f2d
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-05-12 01:45:34 +0300

    гшн

--- homework3.py
+++ homework3.py
@@ -1,7 +1,8 @@
 first = 52
-def function(second):
+def function(second=2):
     global first
     first = second + 10
+    return first
     def function2(number):
         second = 12
         return second
