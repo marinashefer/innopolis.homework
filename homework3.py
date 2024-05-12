4fbf4e425b2353cc28d9d825a95f9f7e05936dbd
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-05-12 18:02:02 +0300

    !

--- homework3.py
+++ homework3.py
@@ -1,11 +1,8 @@
 first = 52
-def function(second=2):
-    global first
-    first = second + 10
-    return first
-    def function2(number):
-        second = 12
-        return second
+second = 15
 
-summa = first + second
-print('Сумма:', summa)
+def function(first, third):
+    global second
+    end = first + second + third
+    print('Сумма: ', end)
+function(40, 20)
---
+++ testiruem.py
@@ -0,0 +1,11 @@
+first = 52
+second = 15
+
+def function():
+    global second, first
+    end = first + second
+    print(end)
+    return end
+
+
+print('Сумма:', end)
