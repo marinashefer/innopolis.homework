064f43c1423ad9b7d0a8bc678d18c4dde82faeeb
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-05-13 18:16:15 +0300

    conm

---
+++ collection.py
@@ -0,0 +1,15 @@
+#списки
+numbers = [2, 4, 6, 8, 10]
+numbers = ['Python', 'C++', 'Java', 'JavaScript']
+info = ['Python', 2022, 24.2]
+
+#numbers[0] == 2
+
+print(numbers[0], numbers[3])
+
+my_list = list()
+new_list = []
+print(new_list)
+
+numbers = list(range(10))
+print(numbers)
