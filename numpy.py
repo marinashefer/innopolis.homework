9b867eb14d4a4481c261e6787e11ca5e1ada730c
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-05-22 18:39:50 +0300

    h,

--- numpy.py
+++ numpy.py
@@ -78,3 +78,78 @@ print(a)
 #k - элемент с которого начинается заполнение единицами
 a = np.eye(4, k=1, dtype=float)
 print(a)
+
+a = np.array([1, 2, 3], float)
+b = np.array([1, 2, 3], float)
+print(a*b)
+
+a = np.array([[1, 2, 3],[4, 5, 6]], float)
+b = np.array([[1, 2, 3],[2, 7, 8]], float)
+print(a*b)
+
+#меньший массив используется несколько раз при умножении
+
+a = np.zeros((2, 2), float)
+b = np.array([-1.,3.], float)
+print(a+b)
+
+#floor, ceil, rint
+#floor - округляет в меньшую сторону
+#ceil - округляет в большую сторону
+#rint - округляет по правилам
+a = np.array([1.1, 1.4, 1.9], float)
+print(a)
+print(np.floor(a))
+
+a = np.array([1, 2, 3], float)
+for x in a:
+    print(x)
+
+a = np.array([[1, 2,], [8, 3], [4, 6]], float)
+for x in a:
+    print(x)
+
+#перебируем массив через цикл
+a = np.array([[1, 2,], [8, 3], [4, 6]], float)
+for (x, y) in a:
+    print(x * y)
+
+#суммируем - sum
+a = np.array([6, 2, 9], float)
+print(a.sum())
+
+#перемножаем - prod
+a = np.array([6, 2, 9], float)
+print(a.prod())
+
+#среднее значение - mean
+a = np.array([6, 2, 9], float)
+print(a.mean())
+
+#вариация-дисперсия(на сколько сильно друг от друга разбросаны значения) - var !для фигур!
+a = np.array([6, 2, 9], float)
+print(a.var())
+
+#девариация - std
+a = np.array([6, 2, 9], float)
+print(a.std())
+
+a = np.array([6, 2, 9], float)
+print(a.max())
+
+a = np.array([6, 2, 9], float)
+print(a.argmin())
+
+a = np.array([6, 2, -1, 0, 9], float)
+print(sorted(a))
+
+a = np.array([6, -1, 0, 2, 9], float)
+print(a.clip(0, 2))
+
+#вытаскиваем уникальные элементы
+a = np.array([6, 6, 2, -1, -1, 2, 9], float)
+print(np.unique())
+
+#выводим диагональ массива
+a = np.array([[6, 4], [3, 5]], float)
+print(a.diagonal())
