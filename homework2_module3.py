92ce7e8e5aae84c176acee37825cc259dd1af542
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-07-29 00:43:04 +0300

    commir

---
+++ homework2_module3.py
@@ -0,0 +1,50 @@
+# создайте классификатор фото одежды с использованием Tensorflow и набора данных Fashion-MNIST
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# проверим первое изображение на тренировочном наборе
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+
+# масштабируем эти значения от 0 до 1
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# отображаем первые 25 изображений
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+
+# строим модель
+model = keras.Sequential([keras.layers.Flatten(input_shape=(28, 28)), keras.layers.Dense(128, activation=tf.nn.relu), keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+plt.figure(figsize=(10, 10))
+for i in range(5):
+    plt.subplot(5, 2, 2 * i + 1)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.title("prediction")
+    plt.axis('off')
